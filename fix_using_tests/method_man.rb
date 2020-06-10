19b32c88f984b8e579a4d7377add6fa862486b3f
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-10 18:08:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
-
-def start_game(Rocky, Bullwinkle)
-  "Hello #{Rocky} & #{Bullwinkle}"
+def start_game(player1, player2)
+  player1=Rocky
+  player2=Bullwinkle
+  "Hello #{player1} & #{player2}"
 end
 
 
