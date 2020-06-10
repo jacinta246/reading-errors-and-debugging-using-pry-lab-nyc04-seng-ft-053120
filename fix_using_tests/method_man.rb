6b8307bc0d1d1c9f54099c0b437a30aefeb4c62a
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-10 18:07:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  player1==Rocky
-  player2==Bullwinkle
+  player1=Rocky
+  player2=Bullwinkle
   "Hello #{player1} & #{player2}"
 end
 
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
